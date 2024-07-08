ae92b821f23d1abc350c098c0b8261f241c974fe
Matthew Heguy <10162554+mheguy@users.noreply.github.com>
2024-07-08 01:32:40 +0000

    Add audio processing (transcription and diarization)

--- sgu/config.py
+++ sgu/config.py
@@ -4,3 +4,6 @@ WIKI_EPISODE_URL_BASE = "https://www.sgutranscripts.org/w/rest.php/v1/page/SGU_E
 
 # Custom headers to identify ourselves to the SGU servers.
 CUSTOM_HEADERS = {"User-Agent": "SGU Wiki Tooling (matthew.heguy@gmail.com)"}
+
+TRANSCRIPTION_MODEL = "medium.en"
+TRANSCRIPTION_LANGUAGE = "en"
