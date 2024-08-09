603106b6472fc066c3650e57afe8ea3218e9a8fd
Matthew Heguy <10162554+mheguy@users.noreply.github.com>
2024-08-09 19:42:48 -0400

    Revert "Revert logging changes (issue was in gcp)"
    This reverts commit 698a2ceb32546e0ca31ed7e4ee9ffd2cbcb7fd24.

--- transcription_bot/global_logger.py
+++ transcription_bot/global_logger.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from loguru import logger
@@ -6,10 +7,16 @@ from transcription_bot.config import LOG_LEVEL
 
 logger.remove()
 
-
 logger.add(
-    sys.stdout,
+    sys.stderr,
     format="{time:HH:mm:ss} <level>{level: <8}</level> [transcript-bot] {message}",
     level=LOG_LEVEL,
     colorize=True,
 )
+
+
+if os.getenv("GOOGLE_VM_CONFIG_LOCK_FILE"):
+    from logging.handlers import SysLogHandler
+
+    handler = SysLogHandler(address="/dev/log")
+    logger.add(handler, colorize=False, level=LOG_LEVEL, format="transcribe-bot[0000]: {level: <8} {message}")
