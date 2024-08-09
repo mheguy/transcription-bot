6a9b741a6664a333350a6811a90799aa0579f92a
Matthew Heguy <10162554+mheguy@users.noreply.github.com>
2024-08-09 18:55:54 -0400

    Add syslog logging

--- transcription_bot/global_logger.py
+++ transcription_bot/global_logger.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from loguru import logger
@@ -6,10 +7,17 @@ from transcription_bot.config import LOG_LEVEL
 
 logger.remove()
 
+log_format = "{time:HH:mm:ss} <level>{level: <8}</level> [transcript-bot] {message}"
 
 logger.add(
     sys.stderr,
-    format="{time:HH:mm:ss} <level>{level: <8}</level> [transcript-bot] {message}",
+    format=log_format,
     level=LOG_LEVEL,
     colorize=True,
 )
+
+if os.getenv("GCE_METADATA_HOST"):
+    from logging.handlers import SysLogHandler
+
+    handler = SysLogHandler(address="/dev/log")
+    logger.add(handler)
