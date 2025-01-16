2b1de9df45b2c06c5021774d7b68f2107e907e62
mheguy <10162554+mheguy@users.noreply.github.com>
2025-01-16 15:51:35 -0500

    Add bot-detection evasion to retrieve article titles (JA3 modification)

--- transcription_bot/utils/global_http_client.py
+++ transcription_bot/utils/global_http_client.py
@@ -1,13 +1,28 @@
 import logging
+from dataclasses import dataclass
 from typing import Any, override
 
 import requests
+import tls_client
 from tenacity import before_sleep_log, retry, stop_after_attempt, wait_fixed
+from tls_client.exceptions import TLSClientExeption
 
 __all__ = ["http_client"]
 
 _CUSTOM_HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0"}
 _HTTP_TIMEOUT = 15
+
+
+@dataclass
+class MockRawResponse:
+    content: bytes | None
+
+    def read(self, _) -> bytes | None:
+        if content := self.content:
+            self.content = None
+            return content
+
+        return None
 
 
 class HttpClient(requests.Session):
@@ -23,7 +38,13 @@ class HttpClient(requests.Session):
 
     @override
     def get(self, *args: Any, raise_for_status: bool = True, **kwargs: Any) -> requests.Response:
-        return self._request("GET", *args, raise_for_status=raise_for_status, **kwargs)
+        try:
+            return self._request("GET", *args, raise_for_status=raise_for_status, **kwargs)
+        except Exception:
+            if resp := get_with_evasion(*args, raise_for_status=raise_for_status, **kwargs):
+                return resp
+
+            raise
 
     @override
     def post(self, *args: Any, raise_for_status: bool = True, **kwargs: Any) -> requests.Response:
@@ -48,6 +69,27 @@ class HttpClient(requests.Session):
             response.raise_for_status()
 
         return response
+
+
+def get_with_evasion(*args: Any, raise_for_status: bool, **kwargs: Any) -> requests.Response | None:
+    """Get a webpage while simulating a browser."""
+    try:
+        resp = tls_client.Session().execute_request("GET", *args, **kwargs)
+    except TLSClientExeption:
+        return None
+
+    requests_resp = requests.Response()
+    requests_resp.status_code = resp.status_code or 400
+
+    if raise_for_status and not requests_resp.ok:
+        return None
+
+    if not isinstance(resp.content, bytes):
+        return None
+
+    requests_resp.raw = MockRawResponse(resp.content)
+
+    return requests_resp
 
 
 http_client = HttpClient()
